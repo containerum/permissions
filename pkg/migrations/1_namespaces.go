57f3da5d8dc125ca27285fde28f78270a98e38ce
Vladimir Stolyarov <xakep6666@gmail.com>
2018-04-18 14:10:50 +0300

    Add model and migration for namespaces

---
+++ pkg/migrations/1_namespaces.go
@@ -0,0 +1,25 @@
+package migrations
+
+import (
+	"git.containerum.net/ch/permissions/pkg/model"
+	"github.com/go-pg/migrations"
+	"github.com/go-pg/pg/orm"
+)
+
+func init() {
+	migrations.Register(func(db migrations.DB) error {
+		if _, err := db.Exec( /* language=sql */ `CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`); err != nil {
+			return err
+		}
+
+		_, err := db.Model(model.Namespace{}).CreateTable(&orm.CreateTableOptions{IfNotExists: true})
+		return err
+	}, func(db migrations.DB) error {
+		if _, err := db.Model(model.Namespace{}).DropTable(&orm.DropTableOptions{IfExists: true}); err != nil {
+			return err
+		}
+
+		_, err := db.Exec( /* language=sql */ `DROP EXTENSION IF EXISTS "uuid-ossp"`)
+		return err
+	})
+}
---
+++ pkg/model/resource.go
@@ -0,0 +1,13 @@
+package model
+
+import "time"
+
+type Resource struct {
+	ID          string     `sql:"id,pk,type:uuid,default:uuid_generate_v4()"`
+	CreateTime  time.Time  `sql:"create_time,default:now(),type:TIMESTAMPTZ"`
+	Deleted     bool       `sql:"deleted,notnull"`
+	DeleteTime  *time.Time `sql:"delete_time,type:TIMESTAMPTZ"`
+	TariffID    string     `sql:"tariff_id,type:uuid,notnull"`
+	OwnerUserID string     `sql:"owner_user_id,type:uuid,notnull"`
+	Label       string     `sql:"label,notnull"`
+}
