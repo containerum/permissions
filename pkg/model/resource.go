fee48d8e578106064a0845875d5d06d227184a4e
Vladimir Stolyarov <xakep6666@gmail.com>
2018-04-18 20:05:45 +0300

    Add hooks BeforeDelete and BeforeUpdate

--- pkg/model/resource.go
+++ pkg/model/resource.go
@@ -1,6 +1,11 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/go-pg/pg/orm"
+	"github.com/pkg/errors"
+)
 
 type Resource struct {
 	ID          string     `sql:"id,pk,type:uuid,default:uuid_generate_v4()"`
@@ -10,4 +15,17 @@ type Resource struct {
 	TariffID    string     `sql:"tariff_id,type:uuid,notnull"`
 	OwnerUserID string     `sql:"owner_user_id,type:uuid,notnull"`
 	Label       string     `sql:"label,notnull"`
+}
+
+func (r *Resource) BeforeDelete(db orm.DB) error {
+	// do not allow delete from app
+	return errors.New("record delete not allowed, use update set deleted = true")
+}
+
+func (r *Resource) BeforeUpdate(db orm.DB) error {
+	if r.Deleted {
+		now := time.Now()
+		r.DeleteTime = &now
+	}
+	return nil
 }
