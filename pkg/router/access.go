dcbc2b08b22874a6e81054a90c64266a8ee9bcad
Vladimir Stolyarov <xakep6666@gmail.com>
2018-04-20 12:04:59 +0300

    Make handler for get user accesses

--- pkg/router/access.go
+++ pkg/router/access.go
@@ -1,9 +1,31 @@
 package router
 
 import (
+	"net/http"
+
 	"git.containerum.net/ch/permissions/pkg/server"
+	"git.containerum.net/ch/utils/httputil"
+	"github.com/gin-gonic/gin"
 )
 
-func (r *Router) SetupAccessRoutes(acts server.AccessActions) {
+type accessHandlers struct {
+	tv   *TranslateValidate
+	acts server.AccessActions
+}
 
+func (ah *accessHandlers) getUserAccessesHandler(ctx *gin.Context) {
+	userID := httputil.MustGetUserID(ctx)
+	ret, err := ah.acts.GetUserAccesses(ctx.Request.Context(), userID)
+	if err != nil {
+		ctx.AbortWithStatusJSON(ah.tv.HandleError(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, ret)
+}
+
+func (r *Router) SetupAccessRoutes(acts server.AccessActions) {
+	handlers := &accessHandlers{acts: acts, tv: r.tv}
+
+	r.engine.GET("/access", handlers.getUserAccessesHandler)
 }
--- pkg/server/types.go
+++ pkg/server/types.go
@@ -1,4 +1,11 @@
 package server
 
+import (
+	"context"
+
+	"git.containerum.net/ch/auth/proto"
+)
+
 type AccessActions interface {
+	GetUserAccesses(ctx context.Context, userID string) ([]*authProto.AccessObject, error)
 }
