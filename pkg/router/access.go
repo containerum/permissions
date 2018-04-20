98c2c4b66609d1eb951f54f862eaad3d87f1171c
Vladimir Stolyarov <xakep6666@gmail.com>
2018-04-20 11:16:33 +0300

    Router and server draft

---
+++ pkg/router/access.go
@@ -0,0 +1,9 @@
+package router
+
+import (
+	"git.containerum.net/ch/permissions/pkg/server"
+)
+
+func (r *Router) SetupAccessRoutes(acts server.AccessActions) {
+
+}
---
+++ pkg/router/types.go
@@ -0,0 +1,73 @@
+package router
+
+import (
+	"net/textproto"
+
+	"git.containerum.net/ch/kube-client/pkg/cherry"
+	"git.containerum.net/ch/permissions/pkg/errors"
+	"git.containerum.net/ch/utils/httputil"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/universal-translator"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+type TranslateValidate struct {
+	*ut.UniversalTranslator
+	*validator.Validate
+}
+
+func (tv *TranslateValidate) HandleError(err error) (int, *cherry.Err) {
+	switch err.(type) {
+	case *cherry.Err:
+		e := err.(*cherry.Err)
+		return e.StatusHTTP, e
+	default:
+		return errors.ErrInternal().StatusHTTP, errors.ErrInternal().AddDetailsErr(err)
+	}
+}
+
+func (tv *TranslateValidate) BadRequest(ctx *gin.Context, err error) (int, *cherry.Err) {
+	if validationErr, ok := err.(validator.ValidationErrors); ok {
+		ret := errors.ErrRequestValidationFailed()
+		for _, fieldErr := range validationErr {
+			if fieldErr == nil {
+				continue
+			}
+			t, _ := tv.FindTranslator(httputil.GetAcceptedLanguages(ctx.Request.Context())...)
+			ret.AddDetailF("Field %s: %s", fieldErr.Namespace(), fieldErr.Translate(t))
+		}
+		return ret.StatusHTTP, ret
+	}
+	return errors.ErrRequestValidationFailed().StatusHTTP, errors.ErrRequestValidationFailed().AddDetailsErr(err)
+}
+
+func (tv *TranslateValidate) ValidateHeaders(headerTagMap map[string]string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		headerErr := make(map[string]validator.ValidationErrors)
+		for header, tag := range headerTagMap {
+			ferr := tv.VarCtx(ctx.Request.Context(), ctx.GetHeader(textproto.CanonicalMIMEHeaderKey(header)), tag)
+			if ferr != nil {
+				headerErr[header] = ferr.(validator.ValidationErrors)
+			}
+		}
+		if len(headerErr) > 0 {
+			ret := errors.ErrRequestValidationFailed()
+			for header, fieldErrs := range headerErr {
+				for _, fieldErr := range fieldErrs {
+					if fieldErr == nil {
+						continue
+					}
+					t, _ := tv.FindTranslator(httputil.GetAcceptedLanguages(ctx.Request.Context())...)
+					ret.AddDetailF("Header %s: %s", header, fieldErr.Translate(t))
+				}
+			}
+			ctx.AbortWithStatusJSON(ret.StatusHTTP, ret)
+			return
+		}
+	}
+}
+
+type Router struct {
+	gin.IRouter
+	*TranslateValidate
+}
